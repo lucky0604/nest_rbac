575f74dfab9970f550bb00713deb30f8a21fe326
Lucky <lucky_soft@163.com>
2020-01-03 18:41:37 +0800

    add modules entities

---
+++ nestjs_be/src/modules/entities/control.entity.ts
@@ -0,0 +1,79 @@
+import {Entity, Column, PrimaryColumn, ManyToOne, ObjectType} from 'typeorm'
+import {Page} from './page.entity'
+import {Select} from '../../common/interfaces/select.interface'
+
+@Entity('system_control')
+export class Control {
+  @PrimaryColumn('uuid', {length: 36, type: 'char'})
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  code: string
+
+  @Column({nullable: true})
+  description: string
+
+  @Column({nullable: true})
+  required?: boolean
+
+  @Column({nullable: true})
+  disabled?: boolean
+
+  @Column({nullable: true})
+  readonly?: boolean
+
+  @Column({nullable: true})
+  hide?: boolean
+
+  @Column({nullable: null})
+  primary?: boolean
+
+  @Column()
+  sort: number
+
+  @Column({nullable: true, type: 'json'})
+  col?: ColType
+
+  @Column({type: 'json'})
+  type: ControlType
+
+  @Column({nullable: true, type: 'json'})
+  group?: Object
+
+  @Column({length: 36, type: 'char'})
+  pageId: string
+
+  @ManyToOne(type => Page, page => page.controls, {onDelete: 'CASCADE'})
+  page: Page
+}
+
+export interface ColType extends Select<ColEnum> {}
+
+export enum ColEnum {
+  One = 1,
+  Two = 2,
+  Three = 3,
+  Four = 4,
+  Five = 5,
+  Six = 6,
+  Seven = 7,
+  Eight = 8,
+  Nine = 9,
+  Ten = 10,
+  Eleven = 11,
+  Twelve = 12
+}
+
+export interface ControlType extends Select<ControlEnum> {}
+
+export enum ControlEnum {
+  Input = 'input',
+  Checkbox = 'checkbox',
+  Buttons = 'buttons',
+  Select = 'select',
+  Findback = 'findback',
+  AddItem = 'add-item'
+}

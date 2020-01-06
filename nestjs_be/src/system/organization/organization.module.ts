5f12814178f38d1032ae5e4af51acd6825846638
Lucky <lucky_soft@163.com>
2020-01-06 16:52:30 +0800

    add organization module

---
+++ nestjs_be/src/system/organization/organization.module.ts
@@ -0,0 +1,18 @@
+import {Module} from '@nestjs/common'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {Organization} from './entities/organization.entity'
+import {OrganizationService} from './services/organization.service'
+import {OrganizationController} from './organization.controller'
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Organization])
+  ],
+  controllers: [
+    OrganizationController
+  ],
+  providers: [
+    OrganizationService
+  ]
+})
+export class OrganizationModule {}

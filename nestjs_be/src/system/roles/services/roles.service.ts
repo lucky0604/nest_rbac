1eb9dda9e9e6fa05983908cf1b5008a6a1dee8e6
Lucky <lucky_soft@163.com>
2020-01-06 16:27:43 +0800

    add roles module

---
+++ nestjs_be/src/system/roles/services/roles.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@nestjs/common'
+import {InjectRepository} from '@nestjs/typeorm'
+import {Repository, ObjectID} from 'typeorm'
+import {RepositoryService} from '../../../common/services/repository.service'
+import {Role} from '../entities/role.entity'
+
+@Injectable()
+export class RolesService extends RepositoryService<Role> {
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>
+  ) {
+    super(roleRepository)
+  }
+
+  async findOne(id: string | number | Date | ObjectID): Promise<Role> {
+    return await this.roleRepository.findOne(id, {relations: ['actions']})
+  }
+}

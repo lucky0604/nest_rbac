4d1ae1aa6882d838c9350289815bc2745c7efb50
Lucky <lucky_soft@163.com>
2020-01-06 17:59:19 +0800

    add action module

---
+++ nestjs_be/src/system/actions/action.controller.ts
@@ -0,0 +1,13 @@
+import {Controller, UseGuards} from '@nestjs/common'
+import {ControllerService} from '../../common/services/controller.service'
+import {Action} from './entities/action.entity'
+import {ActionsService} from './services/actions.service'
+import {AuthGuard} from '@nestjs/passport'
+
+@Controller('actions')
+@UseGuards(AuthGuard('jwt'))
+export class ActionsController extends ControllerService<Action> {
+  constructor(private readonly entityService: ActionsService) {
+    super(entityService)
+  }
+}
---
+++ nestjs_be/src/system/actions/services/actions.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@nestjs/common'
+import {InjectRepository} from '@nestjs/typeorm'
+import {Repository, ObjectID} from 'typeorm'
+import {RepositoryService} from '../../../common/services/repository.service'
+import {Action} from '../entities/action.entity'
+import {ResultList} from '../../../common/interfaces/result.interface'
+
+export interface ActionQuery {
+  menuId: string
+}
+
+@Injectable()
+export class ActionsService extends RepositoryService<Action> {
+  constructor(
+    @InjectRepository(Action)
+    private readonly entityRepository: Repository<Action>
+  ) {
+    super(entityRepository)
+  }
+
+  async findAll(index: number, size: number, query: ActionQuery): Promise<ResultList<Action>> {
+    return new Promise<ResultList<Action>>(async (x) => {
+      let querys = this.entityRepository.createQueryBuilder('action')
+      if (query.menuId) {
+        querys = querys.where('action.menuId = :id', {id: query.menuId})
+      }
+      let result: ResultList<Action> = {
+        list: await querys.skip(size * (index - 1)).take(size).getMany(),
+        count: await querys.getCount(),
+        query: {
+          index: index,
+          size: size
+        }
+      }
+
+      return result
+    })
+  }
+}
